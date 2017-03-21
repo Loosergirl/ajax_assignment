0b269ea7975e211344ee12c6d89e0b7106f6de6c
Maria Kilsved <Maria Kilsved>
2017-03-21 14:17:49 +0100

    Simple search

--- js/main.js
+++ js/main.js
@@ -1,3 +1,65 @@
+var stickers = (function () {
+    /*Replace blankspace with '+' in a string*/
+    var searchStringReplacement = (str) => {
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] === " ") {
+                str[i] = "+";
+            }
+        }
+        return str;
+    };
+
+    /*Return value of search field with blankspaces converted to '+'*/
+    var determineSearchTerms = () => {
+        var terms = document.getElementById('search').value;
+        terms = searchStringReplacement(terms);
+        return terms;
+    };
+
+    var addSearchEventListener = () => {
+        document.getElementById('submit').addEventListener('click', search);
+    };
+
+    var embedAll = (response) => {
+        for (let i = 0; i < response.data.length; i++) {
+            var gif = document.createElement("img");
+            gif.setAttribute("src", response.data[i].images.fixed_height_small.url);
+            gif.setAttribute("alt", "gif");
+            document.body.appendChild(gif);
+        }
+    };
+
+    /*Ajax request which uses search method of the API. Returns an URL for embedding.*/
+    var search = () => {
+        var url = `http://api.giphy.com/v1/stickers/search?q=${determineSearchTerms()}&limit=28&api_key=dc6zaTOxFJmzC`;
+
+        $.ajax({
+            method: 'GET',
+            url: url,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: (response) => {
+                console.log(response);
+                embedAll(response);
+                return response;
+            },
+            error: (error) => {
+                console.log(error);
+                return error;
+            }
+        })
+    };
+
+    /*Public*/
+    return {
+        addSearchEventListener: addSearchEventListener
+    };
+})();
+
+stickers.addSearchEventListener();
+
+
+/*
 function getAll() {
 	$.ajax({
 		method: 'GET',
@@ -91,3 +153,4 @@ function exterminate() {
 		}
 	})
 }
+*/
